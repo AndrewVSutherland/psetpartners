6bf0bbc380f27634e1fbd7d72bf440afc5be0b5c
AndrewVSutherland <drew@math.mit.edu>
2020-08-26 10:18:13 -0400

    enable group creation

--- psetpartners/group.py
+++ psetpartners/group.py
@@ -2,7 +2,7 @@ from . import db
 
 def generate_group_name():
     while True:
-        noun = db.nouns.random()
+        noun = db.plural_nouns.random()
         name = db.adjectives.random({'firstletter':noun[0]}).capitalize() + " " + noun.capitalize()
         if db.groups.lucky({'group_name':{'$ilike':name}}) is None:
             return name
