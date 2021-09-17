e01b0206262d7b84fed3b0412e48a1704bb0e8dc
AndrewVSutherland <drew@math.mit.edu>
2021-09-17 19:19:28 -0400

    fix date bug and add matchjob

--- matchjob.py
+++ matchjob.py
@@ -1,5 +1,5 @@
-from psetpartners.admin import make_matches, send_poolme
+from psetpartners.admin import make_matches, send_poolme_links, send_checkins
 make_matches(forcelive=True)
-pool_me(forcelive=True,preview=False)
+send_poolme_links(forcelive=True,preview=False)
 send_checkins(forcelive=True,preview=False)
 exit
